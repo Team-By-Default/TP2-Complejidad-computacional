8f4719e3b8fb5f6b102aa283c116157534ec7563
Valeria Silvestri <valeria.ayelen@gmail.com>
2017-09-13 18:21:09 -0300

    Poniendo todo
    Tratando de meter mi proyecto

---
+++ src/complejidadComputacional/Polinomio.java
@@ -0,0 +1,24 @@
+package complejidadComputacional;
+
+public class Polinomio {
+	
+	private int grado ;
+	private double [ ] coeficientes;
+	//La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n y la	posicion n contiene al termino independiente.
+	
+	public Polinomio() { }
+	
+	/*double evaluarMSucesivas (double x) {}
+	double evaluarRecursiva (double x) {}
+	double evaluarRecursivaPar (double x) {}
+	double evaluarProgDinamica (double x) {}
+	double evaluarMejorada (double x) {}
+	double evaluarPow (double x) {}
+	double evaluarHorner (double x) {}
+	*/
+	
+	public static void main(String[] args) {
+
+	}
+
+}
