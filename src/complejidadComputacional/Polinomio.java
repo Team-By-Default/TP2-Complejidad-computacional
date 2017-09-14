e3372305d06a25525e95f6f1c0adc869f9d2ed28
Valeria Silvestri <valeria.ayelen@gmail.com>
2017-09-13 21:40:40 -0300

    Constructor y Recursiva
    Completado el constructor y evaluarRecursiva()

--- src/complejidadComputacional/Polinomio.java
+++ src/complejidadComputacional/Polinomio.java
@@ -6,20 +6,52 @@ public class Polinomio {
 	private double [ ] coeficientes;
 	//La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n y la	posicion n contiene al termino independiente.
 	
-	public Polinomio() { }
+	public Polinomio(int g, double[] coef) {
+		this.grado=g;
+		this.coeficientes = new double [this.grado+1];
+		for(int i=0; i<=this.grado; i++)
+			this.coeficientes[i] = coef[i];
+	}
 	
 	/* Antes de empezar a trabajar con una función, anotemos en un comentario al lado quién la va a hacer
-	double evaluarMSucesivas (double x) {}
-	double evaluarRecursiva (double x) {}
-	double evaluarRecursivaPar (double x) {} //Vale
-	double evaluarProgDinamica (double x) {}
+	double evaluarMSucesivas (double x) {} */
+	
+	public double evaluarRecursiva (double x) {
+		double result=0;
+		int n = this.grado;
+		for(int i=0; i<=this.grado; i++) {
+			result += this.coeficientes[i] * potencia(x,n);
+			n--;
+		}
+		return result;
+	}
+	
+	private double potencia(double x, int n) {
+		if(n<0)
+			throw new potenciaException("Potencia negativa");
+		if(n==0)
+			return 1;
+		if(n==1)
+			return x;
+		return x*potencia(x, n-1);
+	}
+	
+	/*double evaluarRecursivaPar (double x) {
+		
+	}*/
+	
+	/*
+	 * double evaluarProgDinamica (double x) {}
 	double evaluarMejorada (double x) {}
 	double evaluarPow (double x) {}
 	double evaluarHorner (double x) {}
 	*/
 	
 	public static void main(String[] args) {
-
+		double coef[]= {1,2,3};
+		Polinomio poly = new Polinomio(2,coef);
+		System.out.println(poly.evaluarRecursiva(1));
+		System.out.println(poly.evaluarRecursiva(-1));
 	}
 
 }
