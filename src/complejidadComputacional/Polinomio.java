e23bbcc93dec7ae926062ff7f388e82e1db0f800
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-13 22:53:58 -0300

    Hice un constructor

--- src/complejidadComputacional/Polinomio.java
+++ src/complejidadComputacional/Polinomio.java
@@ -6,10 +6,15 @@ public class Polinomio {
 	private double [ ] coeficientes;
 	//La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n y la	posicion n contiene al termino independiente.
 	
-	public Polinomio() { }
+	public Polinomio(int g, double vec[]) { 
+		this.grado=g;
+		for(int i=0;i<=this.grado;i++) {
+			this.coeficientes[i]=vec[i];
+		}
+	}
 	
 	/* Antes de empezar a trabajar con una función, anotemos en un comentario al lado quién la va a hacer
-	double evaluarMSucesivas (double x) {}
+	double evaluarMSucesivas (double x) {} //Ale
 	double evaluarRecursiva (double x) {}
 	double evaluarRecursivaPar (double x) {} //Vale
 	double evaluarProgDinamica (double x) {}
@@ -17,6 +22,15 @@ public class Polinomio {
 	double evaluarPow (double x) {}
 	double evaluarHorner (double x) {}
 	*/
+	
+	double evaluarMSucesivas(double x) {
+		double pow=1,total=0;
+		for(int i=this.grado;i>=0;i--) {
+			total=total+pow*this.coeficientes[i];
+			pow=pow*x;
+		}
+		return total;	
+	}
 	
 	public static void main(String[] args) {
 
