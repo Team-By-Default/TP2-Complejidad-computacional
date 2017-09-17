9f1e528983707a055b9a41c2d12ea916b06411c2
Lara Jazmin Cavicchioli <laracavic132@gmail.com>
2017-09-17 13:40:49 -0300

    test de velocidad en binomio

--- test/complejidadComputacional/BinomioDeNewtonTest.java
+++ test/complejidadComputacional/BinomioDeNewtonTest.java
@@ -1,5 +1,15 @@
 package complejidadComputacional;
 
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+
 public class BinomioDeNewtonTest {
 
+	@Before
+	public void SetUp(){
+		BinomioDeNewton bin=new BinomioDeNewton(2,3);
+	}
+	
+	
 }
