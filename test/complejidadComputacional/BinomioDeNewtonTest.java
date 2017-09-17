ecaebb0fd729d00ea57229804253a637e7c37c3e
Lara Jazmin Cavicchioli <laracavic132@gmail.com>
2017-09-17 19:17:55 -0300

    testeo de velocidad de binomio
    done

--- test/complejidadComputacional/BinomioDeNewtonTest.java
+++ test/complejidadComputacional/BinomioDeNewtonTest.java
@@ -8,7 +8,7 @@ import org.junit.After;
 public class BinomioDeNewtonTest {
 
 	
-	int intentos=1;
+	int intentos=10000;
 	long inicio;
 	BinomioDeNewton bin;
 	int nBajo=1,
@@ -20,7 +20,7 @@ public class BinomioDeNewtonTest {
 		bin=new BinomioDeNewton(2,3);
 	}
 	
-	
+	/*
 	//CoeficienteDelTerminoK
 	@Test
 	public void pruebaSimpleCoefDelTerminoK() {
@@ -149,8 +149,131 @@ public class BinomioDeNewtonTest {
 			}
 			System.out.print("Coeficiente de K Largo DIN TD: ");
 		}
+	*/
 	
+	//CoeficientesK
+		@Test
+		public void pruebaSimpleCoeficientesK() {
+			inicio = System.currentTimeMillis();    
+			for(int j=0;j<intentos;j++){
+	        	for (int i = 0; i < nBajo; ++i) 
+		       		bin.obtenerCoeficientesK(i);
+	        }
+			System.out.print("Coeficientes de K Simple: ");		
+		}
+		
+		@Test
+		public void pruebaMedioCoeficientesK() {
+			inicio = System.currentTimeMillis();
+		       for(int j=0;j<intentos;j++){
+		       	for (int i = 0; i < nMedio; ++i) 
+		       		bin.obtenerCoeficientesK(i);
+		       }
+			System.out.print("Coeficientes de K Medio: ");
+		}
+		@Test
+		public void pruebaLargoCoeficientesK() {
+			inicio = System.currentTimeMillis();	
+			for(int j=0;j<intentos;j++){
+		      	for (int i = 0; i < nAlto; ++i) 
+		      		bin.obtenerCoeficientesK(i);
+		       }
+			System.out.print("Coeficientes de K Largo: ");
+		}
+		
+	//CoeficientesKRecursiva
+		@Test
+			public void pruebaSimpleCoeficientesKRec() {
+				inicio = System.currentTimeMillis();    
+				for(int j=0;j<intentos;j++){
+		        	for (int i = 0; i < nBajo; ++i) 
+			       		bin.obtenerCoeficientesKRecursiva(i);
+		        }
+				System.out.print("Coeficientes de K REC Simple: ");		
+			}
+			
+		@Test
+		public void pruebaMedioCoeficientesKRec() {
+			inicio = System.currentTimeMillis();
+		    for(int j=0;j<intentos;j++){
+		       	for (int i = 0; i < nMedio; ++i) 
+		       		bin.obtenerCoeficientesKRecursiva(i);
+		       }
+			System.out.print("Coeficientes de K REC Medio: ");
+		}
+		
+		@Test
+		public void pruebaLargoCoeficientesKRec() {
+			inicio = System.currentTimeMillis();
+     	    for(int j=0;j<intentos;j++){
+			    for (int i = 0; i < nAlto; ++i) 
+			       bin.obtenerCoeficientesKRecursiva(i);
+			}
+			System.out.print("Coeficientes de K REC Largo: ");
+		}
+		
+	//CoeficientesKDinamica
+	@Test
+	public void pruebaSimpleCoeficientesKDin() {
+		inicio = System.currentTimeMillis();    
+		for(int j=0;j<intentos;j++){
+			for (int i = 0; i < nBajo; ++i) 
+				bin.todosCoefDinamica(i);
+		}
+		System.out.print("Coeficientes de K DIN Simple: ");		
+	}
+					
+	@Test
+	public void pruebaMedioCoeficientesKDin() {
+		inicio = System.currentTimeMillis();
+		for(int j=0;j<intentos;j++){
+			for (int i = 0; i < nMedio; ++i) 
+				bin.todosCoefDinamica(i);
+		}
+		System.out.print("Coeficientes de K DIN Medio: ");
+	}
+				
+	@Test
+	public void pruebaLargoCoeficientesKDin() {
+		inicio = System.currentTimeMillis();
+		for(int j=0;j<intentos;j++){
+			for (int i = 0; i < nAlto; ++i) 
+				bin.todosCoefDinamica(i);
+		}
+		System.out.print("Coeficientes de K DIN Largo: ");
+	}
 	
+	//CoeficientesKDinamicaTopDown
+		@Test
+		public void pruebaSimpleCoeficientesKDinTD() {
+			inicio = System.currentTimeMillis();    
+			for(int j=0;j<intentos;j++){
+				for (int i = 0; i < nBajo; ++i) 
+					bin.todosCoefDinamicaTD(i);
+			}
+			System.out.print("Coeficientes de K DIN TD Simple: ");		
+		}
+						
+		@Test
+		public void pruebaMedioCoeficientesKDinTD() {
+			inicio = System.currentTimeMillis();
+			for(int j=0;j<intentos;j++){
+				for (int i = 0; i < nMedio; ++i) 
+					bin.todosCoefDinamicaTD(i);
+			}
+			System.out.print("Coeficientes de K DIN TD Medio: ");
+		}
+					
+		@Test
+		public void pruebaLargoCoeficientesKDinTD() {
+			inicio = System.currentTimeMillis();
+			for(int j=0;j<intentos;j++){
+				for (int i = 0; i < nAlto; ++i) 
+					bin.todosCoefDinamicaTD(i);
+			}
+			System.out.print("Coeficientes de K DIN TD Largo: ");
+		}
+		
 	@After
 	public void end() {
 		System.out.println((System.currentTimeMillis()-inicio) + "ms");
